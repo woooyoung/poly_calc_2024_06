798337ad4807861d258237c51fa6118c6e9f2ecc
woooyoung <axdsw121@gmail.com>
2024-06-25 15:40:38 +0900

    불필요한 바깥쪽의 소괄호 제거

--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -113,4 +113,10 @@ public class CalcTests {
     void t18() {
         assertThat(Calc.run("10 * 20 + 10 + 5 * 2")).isEqualTo(220);
     }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    void t19() {
+        assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
+    }
 }
