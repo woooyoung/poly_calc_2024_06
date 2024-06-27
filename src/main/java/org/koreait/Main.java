344e9b6af14dd286975cef56e4dfca6f79f8149c
woooyoung <axdsw121@gmail.com>
2024-06-27 11:30:59 +0900

    5 - (1 + 5) == -1, 3 * 1 + (1 - (4 * 1 - (1 - 1))), 디버그 표시를 더 직관적으로 변경

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -2,6 +2,23 @@ package org.koreait;
 
 public class Main {
     public static void main(String[] args) {
-        Calc.run("10 - 20 + 30");
+        String exp = "5 + -(9 + 1) * -(8 + 2) + 5";
+
+        int startPos = 4;
+        int endPos = 11;
+
+        String head = exp.substring(0, startPos);
+        String body = "(" + exp.substring(startPos + 1, endPos + 1) + " * -1)";
+        String tail = exp.substring(endPos + 1);
+
+        System.out.println("head: " + head);
+        System.out.println("body: " + body);
+        System.out.println("tail: " + tail);
+
+        System.out.println("exp: " + exp);
+        System.out.printf("exp: ");
+        System.out.printf(head);
+        System.out.printf(body);
+        System.out.printf(tail);
     }
 }
--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -173,4 +173,16 @@ public class CalcTests {
     void t28() {
         assertThat(Calc.run("-(8 + 2) * -(7 + 3) + 5")).isEqualTo(105);
     }
+
+    @Test
+    @DisplayName("5 - (1 + 5) == -1")
+    void t29() {
+        assertThat(Calc.run("5 - (1 + 5)")).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("3 * 1 + (1 - (4 * 1 - (1 - 1))) == 0")
+    void t30() {
+        assertThat(Calc.run("3 * 1 + (1 - (4 * 1 - (1 - 1)))")).isEqualTo(0);
+    }
 }
